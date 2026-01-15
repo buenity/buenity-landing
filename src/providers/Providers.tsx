5caca337ed00ad7fd57704097de835bde26e36e1
victor <cordobav444@gmail.com>
2026-01-15 15:45:32 -0500

    separe el layout

---
+++ src/providers/Providers.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { type ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      forcedTheme="light"
+      enableSystem={false}
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
+
