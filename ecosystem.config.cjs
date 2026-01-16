62288134e2d7e202dba14f21b887706bb37b1c3f
victor <cordobav444@gmail.com>
2026-01-16 01:30:20 -0500

    ajustando el ecositen

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,15 +2,10 @@ module.exports = {
     apps: [
         {
             name: "buenity-landing",
-            script: "pnpm ",
-            args: "start",
+            script: "pnpm start -p 6207",
             cwd: "./",
             instances: 1,
             exec_mode: "fork",
-            env: {
-                NODE_ENV: "production",
-                PORT: 6207,
-            },
             error_file: "./logs/pm2-error.log",
             out_file: "./logs/pm2-out.log",
             log_date_format: "YYYY-MM-DD HH:mm:ss Z",
